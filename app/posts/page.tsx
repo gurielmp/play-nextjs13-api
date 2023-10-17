8ff1571d6402b3f7ad5747c95aaabe690437e8b3
gurielamp <gurielamp@gmail.com>
2023-10-18 05:20:16 +0700

    fetch public api

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,10 +1,32 @@
-import ViewUserButton from "../components/ViewUserButton"
-const Posts = () => {
+import CardList from "../components/Posts/CardList"
+import ViewUserButton from "../components/Posts/ViewUserButton"
+
+const base_url = "https://jsonplaceholder.typicode.com/posts"
+interface Iposts {
+  userId: number
+  id: number
+  title: string
+  body: string
+}
+
+const Posts = async () => {
+  const response = await fetch(base_url)
+  const post: Iposts[] = await response.json()
+
   return (
-    <div className="bg-[#38b3cc]">
+    <>
       <h1>Post page</h1>
-      <ViewUserButton />
-    </div>
+      {post.map((post) => {
+        return (
+          <CardList key={post.id}>
+            <p>{post.id}</p>
+            <i>{post.title}</i>
+            <p>{post.body}</p>
+            <ViewUserButton userId={post.userId} />
+          </CardList>
+        )
+      })}
+    </>
   )
 }
 
